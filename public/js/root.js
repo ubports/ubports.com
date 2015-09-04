caeddf0203fd04942340102a3d943cbaa31816dd
Marius Gripsgard <me@mariogrip.com>
2015-09-05 00:51:31 +0200

    Edited error page

--- public/js/root.js
+++ public/js/root.js
@@ -1,5 +1,11 @@
 var app = angular.module("ubports", []);
 
 app.controller("ctrl", ['$scope', '$http', function($scope) {
-  
+  $scope.error;
+  $scope.toggeError = function() {
+    $scope.error = !$scope.error;
+  }
+  $scope.test = function() {
+    console.log("test");
+  }
 }]);
