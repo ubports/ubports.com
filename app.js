da24d001b5e7743635eed748adf6259189376b31
Marius Gripsgard <mariogrip@ubuntu.com>
2017-05-16 01:13:42 +0200

    Add cache breaker

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var routes = require('./src/routes/index');
 
 var app = express();
 
+app.locals.deployVersion = (new Date).getTime();
+
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
@@ -16,7 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 864000000}));
 
 app.use('/', routes);
 
